c3bae4cd0095c079432c5174ad7d55c063fd9ab5
Bobby Galli <bobbyg603@gmail.com>
2022-12-13 13:58:34 -0500

    feat: medium-feed

--- src/medium-feed.ts
+++ src/medium-feed.ts
@@ -1,64 +1,162 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { LitElement, html, css } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 
 /**
- * An example element.
+ * Displays a collection of Medium article cards.
  *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
+ * @property url - The Medium RSS feed url
+ * @property count - The number of preview cards to display
  */
 @customElement('medium-feed')
 export class MediumFeed extends LitElement {
   static override styles = css`
     :host {
       display: block;
-      border: solid 1px gray;
       padding: 16px;
       max-width: 800px;
     }
+
+    .body {
+      flex-grow: 2;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      color: var(--medium-body-color);
+    }
+
+    .card {
+      cursor: pointer;
+      display: flex;
+      background-color: var(--medium-card-background-color, white);
+      border: var(--medium-card-border, solid 1px lightgray);
+      border-radius: var(--medium-card-border-radius, 3px);
+    }
+
+    .footer {
+      color: var(--medium-footer-color, lightgray);
+    }
+
+    .header {
+      flex-grow: 1;
+      color: var(--medium-header-color);
+    }
+
+    .header h2, .header h3 {
+      margin: 0;
+    }
+
+    .right {
+      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .thumbnail {
+      display: flex;
+    }
+
+    .thumbnail img {
+      height: var(--medium-thumbnail-height, 220px);
+      width: var(--medium-thumbnail-width, 330px);
+      border-top-left-radius: var(--medium-thumbnail-border-left-radius);
+      border-bottom-left-radius: var(--medium-thumbnail-border-left-radius);
+      border-top-right-radius: var(--medium-thumbnail-border-right-radius);
+      border-bottom-right-radius: var(--medium-thumbnail-border-right-radius);
+      object-fit: cover;
+    }
   `;
 
-  /**
-   * The name to say "Hello" to.
-   */
   @property()
-  name = 'World';
+  url = '';
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  @property({ type: Number })
+  count = 10;
+
+  @state()
+  private _state: { posts: MediumPost[] } = { posts: [] };
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.fetchData();
+  }
 
   override render() {
-    return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+    return html`${this._state.posts
+      .slice(0, this.count)
+      .map(post => {
+        const header = post.title;
+        const subheader = post.author;
+        const thumbnail = post.thumbnail;
+        const body = `${this.trimContent(post.content)}...`;
+        const footer = post.categories.join(' ');
+
+        return html`
+          <div class="card" @click=${() => this.cardClick(post.link)}>
+            <div class="left">
+              <div class="thumbnail">
+                <img src="${thumbnail}">
+              </div>
+            </div>
+            <div class="right">
+              <div class="header">
+                <h2>${header}</h2>
+                <h3>${subheader}</h3>
+              </div>
+              <div class="body">
+                ${body}
+              </div>
+              <div class="footer">
+                ${footer}
+              </div>
+            </div>
+          </div>
+          <br>
+        `;
+      })
+      }`;
   }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
+  private cardClick(url: string) {
+    window.open(url, "_blank");
   }
 
-  /**
-   * Formats a greeting
-   * @param name The name to say "Hello" to
-   */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
+  private async fetchData() {
+    const url = `https://api.rss2json.com/v1/api.json?rss_url=${this.url}`;
+    const response = await fetch(url);
+    const json = (await response.json()) as MediumResponse;
+    const posts = json.items;
+
+    this._state = { posts };
   }
+
+  private trimContent(content: string) {
+    return content
+      .split("<p>")
+      .splice(3)
+      .join('')
+      .replace(/<\/?[^>]+(>|$)/g, "")
+      .split(' ')
+      .slice(0, 32)
+      .join(' ');
+  }
+}
+
+interface MediumPost {
+  title: string;
+  link: string;
+  thumbnail: string;
+  categories: string[];
+  pubDate: Date;
+  creator: string;
+  author: string;
+  content: string;
+}
+
+
+interface MediumResponse {
+  items: MediumPost[];
 }
 
 declare global {
